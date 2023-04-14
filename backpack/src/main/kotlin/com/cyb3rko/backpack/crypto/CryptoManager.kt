998ab50bdd0edf8556170fd635a413e6a7e9e5be
Niko Diamadis <niko@cyb3rko.de>
2023-04-15 00:21:20 +0200

    Separate release and debug keystore aliases

--- backpack/src/main/kotlin/com/cyb3rko/backpack/crypto/CryptoManager.kt
+++ backpack/src/main/kotlin/com/cyb3rko/backpack/crypto/CryptoManager.kt
@@ -46,7 +46,7 @@ import kotlin.experimental.and
  * The manager object for cryptographic functions like hashing, encryption and (secure) randoms.
  */
 object CryptoManager {
-    private const val KEYSTORE_ALIAS = "iamsecure"
+    private val KEYSTORE_ALIAS = if (!BuildConfig.DEBUG) "iamsecure" else "iamsecuredebug"
     private const val ENC_ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
     private const val ENC_BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
     private const val ENC_PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
