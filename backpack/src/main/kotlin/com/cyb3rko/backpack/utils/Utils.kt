75141f0d5b9134e655cf61943f90f83775ce76d3
Niko Diamadis <niko@cyb3rko.de>
2023-09-26 18:48:05 +0200

    Add getDrawableCompat util function

--- backpack/src/main/kotlin/com/cyb3rko/backpack/utils/Utils.kt
+++ backpack/src/main/kotlin/com/cyb3rko/backpack/utils/Utils.kt
@@ -21,10 +21,13 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.cyb3rko.backpack.R
@@ -106,6 +109,10 @@ internal fun Context.showDialog(
         }
     }
     builder.show()
+}
+
+fun Context.getDrawableCompat(@DrawableRes id: Int): Drawable {
+    return AppCompatResources.getDrawable(this, id)!!
 }
 
 // Fragment extension functions
