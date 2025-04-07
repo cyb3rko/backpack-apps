4c2439eff67f6b4d9e86d27b1aa07bed096e553d
Niko Diamadis <cyb3rko@pm.me>
2025-04-07 17:33:25 +0200

    chore: update to JVM 21

--- backpack/build.gradle.kts
+++ backpack/build.gradle.kts
@@ -22,11 +22,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
         // fix "Missing class java.lang.invoke.StringConcatFactory"
         // (see https://github.com/Kotlin/kotlinx.serialization/issues/2145)
         freeCompilerArgs = listOf("-Xstring-concat=inline")
