17709629250f9e8a17f36c0721a3e2d026f5d557
Niko Diamadis <cyb3rko@pm.me>
2025-06-15 21:10:40 +0200

    feat: add logging functions

---
+++ backpack/src/main/kotlin/de/cyb3rko/backpack/utils/Log.kt
@@ -0,0 +1,11 @@
+package de.cyb3rko.backpack.utils
+
+import android.util.Log
+
+fun logW(tag: String, message: String) {
+    Log.w(tag, message)
+}
+
+fun logD(tag: String, message: String) {
+    Log.d(tag, message)
+}
